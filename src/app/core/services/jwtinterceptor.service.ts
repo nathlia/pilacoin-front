2b374b60a628efc7f120407024ef297ffa631748
nathalia <nattyfelfer@gmail.com>
2023-02-09 20:32:09 -0300

    origins fixed

--- src/app/core/services/jwtinterceptor.service.ts
+++ src/app/core/services/jwtinterceptor.service.ts
@@ -1,6 +1,6 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 import Swal from 'sweetalert2';
 import { LoginService } from './login.service';
 
@@ -8,10 +8,52 @@ import { LoginService } from './login.service';
   providedIn: 'root'
 })
 export class JwtinterceptorService implements HttpInterceptor{
+  isRefreshing: any;
+  eventBusService: any;
 
   constructor(
     private loginService: LoginService
   ) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const LOGGED_USER = this.loginService.getLoggedUser();
+    const token = LOGGED_USER?.token;
+
+    // if (!environment.production) {
+    //     log.debug('Auth Token', token);
+    // }
+
+    if (!token) {
+      return next.handle(req);
+    }
+
+    const req1 = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${token}`),
+      withCredentials: true,
+    });
+
+    return next.handle(req1).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error instanceof HttpErrorResponse && !req.url.includes('auth/login') && error.status === 401) {
+          return this.handle401Error(req, next);
+        }
+
+        return throwError(() => error);
+      })
+    ) as Observable<HttpEvent<any>>;
+  }
+
+  private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+
+      //if (this.credentialsService.isAuthenticated()) {
+      // this.eventBusService.emit(new EventData('logout', null));
+    }
+
+    return next.handle(request);
+  }
+}
+
   // intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     
   //   const LOGGED_USER = this.loginService.getLoggedUser();
@@ -26,25 +68,25 @@ export class JwtinterceptorService implements HttpInterceptor{
   //   }
   // }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const u = this.loginService.getLoggedUser();
-    if (u) {
-        const jwtRequest = request.clone({setHeaders: {Authorization: 'Bearer ' + u.token}});
-        return next.handle(jwtRequest).pipe(
-            tap((evt) => {}, (err) => {
-                    if ([401, 403].indexOf(err.status) !== -1) {
-                        Swal.fire('Não permitido', '', 'error');
-                        // this.loginService.logout();
-                        // location.reload();
-                    }
-                }
-            )
-        );
-    } else {
-        return next.handle(request).pipe(
-            tap(evt => {
-            })
-        );
-    }
-}
-}
+//   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+//     const u = this.loginService.getLoggedUser();
+//     if (u) {
+//         const jwtRequest = request.clone({setHeaders: {Authorization: 'Bearer ' + u.token}});
+//         return next.handle(jwtRequest).pipe(
+//             tap((evt) => {}, (err) => {
+//                     if ([401, 403].indexOf(err.status) !== -1) {
+//                         Swal.fire('Não permitido', '', 'error');
+//                         // this.loginService.logout();
+//                         // location.reload();
+//                     }
+//                 }
+//             )
+//         );
+//     } else {
+//         return next.handle(request).pipe(
+//             tap(evt => {
+//             })
+//         );
+//     }
+// }
+// }
--- src/app/core/services/login.service.ts
+++ src/app/core/services/login.service.ts
@@ -7,6 +7,18 @@ import { UserAccount } from '../models/user-account.model';
 
 //const baseUrl = "http://localhost:8080/login"
 
+export interface Credentials {
+  // Customize received credentials here
+  username: string;
+  token: string;
+  id: string;
+  role: string;
+  nome: string;
+  expires: Date;
+}
+
+const credentialsKey = 'credentials';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,11 +26,17 @@ export class LoginService {
 
   private readonly API_URL = 'http://localhost:8080/'
   private readonly LOGGED_USER = 'user-logged'
+  
 
   constructor(
     private http: HttpClient,
     private router: Router
-    ) { }
+    ) { 
+      const savedCredentials = sessionStorage.getItem(credentialsKey) || localStorage.getItem(credentialsKey);
+      if (savedCredentials) {
+        this._credentials = JSON.parse(savedCredentials);
+      }
+    }
 
     login(credentials: { password: any, username: any }): Observable<UserAccount> {
       return this.http.post<UserAccount>(this.API_URL+'authenticate', credentials).pipe(
@@ -49,6 +67,8 @@ export class LoginService {
     this.router.navigate(['/login']);
   }
 
+  
+
   getLoggedUser(): UserAccount {    
     const loggedUserString = sessionStorage.getItem(this.LOGGED_USER);
     if (loggedUserString) {
@@ -57,4 +77,47 @@ export class LoginService {
       return null;
     }
   }
-}
+
+  /**
+   * Provides storage for authentication credentials.
+   * The Credentials interface should be replaced with proper implementation.
+   */
+ 
+    private _credentials: Credentials | null = null;
+
+    /**
+     * Checks is the user is authenticated.
+     * @return True if the user is authenticated.
+     */
+    isAuthenticated(): boolean {
+      return !!this.credentials;
+    }
+  
+    /**
+     * Gets the user credentials.
+     * @return The user credentials or null if the user is not authenticated.
+     */
+    get credentials(): Credentials | null {
+      return this._credentials;
+    }
+  
+    /**
+     * Sets the user credentials.
+     * The credentials may be persisted across sessions by setting the `remember` parameter to true.
+     * Otherwise, the credentials are only persisted for the current session.
+     * @param credentials The user credentials.
+     * @param remember True to remember credentials across sessions.
+     */
+    setCredentials(credentials?: Credentials, remember?: boolean) {
+      this._credentials = credentials || null;
+  
+      if (credentials) {
+        const storage = remember ? localStorage : sessionStorage;
+        storage.setItem(credentialsKey, JSON.stringify(credentials));
+      } else {
+        sessionStorage.removeItem(credentialsKey);
+        localStorage.removeItem(credentialsKey);
+      }
+    }
+  }
+
